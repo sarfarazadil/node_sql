635eda107875ed0b90551c2b76682ea7113a4e40
sarfarazadil <sarfarazadil18@gmail.com>
2025-06-18 02:08:42 +0530

    update

--- config/db.js
+++ config/db.js
@@ -2,19 +2,27 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config();
 // Create connection
+// const db = mysql.createConnection({
+//   //   host: process.env.DB_HOST,
+//   //   user: process.env.DB_USER,
+//   //   password: process.env.DB_PASS,
+//   //   database: process.env.DB_NAME,
+//   host: "localhost",
+//   user: "nodeuser",
+//   password: "nodepass",
+//   database: "school_db",
+// });
 const db = mysql.createConnection({
-  //   host: process.env.DB_HOST,
-  //   user: process.env.DB_USER,
-  //   password: process.env.DB_PASS,
-  //   database: process.env.DB_NAME,
-  host: "localhost",
-  user: "nodeuser",
-  password: "nodepass",
-  database: "school_db",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306, // use 16017 in env, fallback 3306
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 // Connect and query
 db.connect((err) => {
+  //HEqxQQBqILrnSxuQhTFyERAsBxzhRigP
   if (err) {
     console.error("atabase connection failed:", err.message);
     return;
